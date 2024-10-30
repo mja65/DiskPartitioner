1dfa03fcf468ca85ebdd8723c74ea8abed5ae0f9
mja65 <supremeturnip@hotmail.com>
2024-10-31 08:01:55 +1100

    Update Set-GUIPartitionNewPosition.ps1

--- Functions/Set-GUIPartitionNewPosition.ps1
+++ Functions/Set-GUIPartitionNewPosition.ps1
@@ -8,11 +8,13 @@ function Set-GUIPartitionNewPosition {
     $AmounttoSetLeft = $Partition.Margin.Left + $AmountMoved   
     $PartitionBoundaries = (Get-GUIPartitionBoundaries -PartitionType $PartitionType -Prefix 'WPF_UI_DiskPartition_Partition_' -ObjecttoCheck $Partition)
 
+    #Write-host "Right boundary is: $($PartitionBoundaries.RightBoundary)"
+
     if ($AmounttoSetLeft -lt $PartitionBoundaries.LeftBoundary){
         $AmounttoSetLeft = $PartitionBoundaries.LeftBoundary
     }
-    if (($AmounttoSetLeft + (Get-GUIPartitionWidth -Partition ((Get-Variable -Name $Script:GUIActions.SelectedMBRPartition).Value))) -gt $PartitionBoundaries.RightBoundary){
-        $AmounttoSetLeft = $PartitionBoundaries.RightBoundary - (Get-GUIPartitionWidth -Partition ((Get-Variable -Name $Script:GUIActions.SelectedMBRPartition).Value))
+    if (($AmounttoSetLeft + (Get-GUIPartitionWidth -Partition $Partition)) -gt $PartitionBoundaries.RightBoundary){
+        $AmounttoSetLeft = $PartitionBoundaries.RightBoundary - (Get-GUIPartitionWidth -Partition $Partition)
     } 
     
     $Partition.Margin = [System.Windows.Thickness]"$AmounttoSetLeft,0,0,0"
