85bfed9298b6e56f54337052f3cb5922e6620370
mja65 <supremeturnip@hotmail.com>
2024-12-10 19:19:39 +1100

    Update WPF_ADFKickstartReporting_OK_Button.ps1

--- UIActions/ADFKickstartReporting/WPF_ADFKickstartReporting_OK_Button.ps1
+++ UIActions/ADFKickstartReporting/WPF_ADFKickstartReporting_OK_Button.ps1
@@ -1,4 +1,4 @@
 $WPF_ADFKickstartReporting_OK_Button.Add_Click({
     $WPF_ADFKickstartReporting.Close() | out-null
-    remove variable -name 'WPF_ADFKickstartReporting*'
+    Remove-Variable -name 'WPF_ADFKickstartReporting*'
 })
