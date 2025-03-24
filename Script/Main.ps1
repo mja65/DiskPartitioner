1536bef86635563ecdcb45cfa6bd0f55ed715cad
mja65 <supremeturnip@hotmail.com>
2025-03-24 15:43:12 +1100

    Add function to retrieve input files

--- Script/Main.ps1
+++ Script/Main.ps1
@@ -44,15 +44,9 @@ Write-Emu68ImagerLog -start
 Confirm-DefaultPaths 
 
 # Get-HSTImagerandHSTAmiga
+# Get-InputFiles
 
 Remove-Variable -Name 'WPF_*'
-
-# Update-InputCSV -PathtoGoogleDrive $Script:Settings.PathtoInputFileSpreadsheet -GidValue 0 -ExistingCSV "$($Script:Settings.InputFiles)\ADFHashes.CSV" 
-# Update-InputCSV -PathtoGoogleDrive $Script:Settings.PathtoInputFileSpreadsheet -GidValue 750546389 -ExistingCSV "$($Script:Settings.InputFiles)\ListofInstallFiles.CSV"
-# Update-InputCSV -PathtoGoogleDrive $Script:Settings.PathtoInputFileSpreadsheet -GidValue 2048180409 -ExistingCSV "$($Script:Settings.InputFiles)\ListofPackagestoInstall.CSV"
-# Update-InputCSV -PathtoGoogleDrive $Script:Settings.PathtoInputFileSpreadsheet -GidValue 1875558855 -ExistingCSV "$($Script:Settings.InputFiles)\RomHashes.CSV"
-# Update-InputCSV -PathtoGoogleDrive $Script:Settings.PathtoInputFileSpreadsheet -GidValue 860542576 -ExistingCSV "$($Script:Settings.InputFiles)\ScreenModes.CSV"
-
 
 $WPF_MainWindow = Get-XAML -WPFPrefix 'WPF_Window_' -XMLFile '.\Assets\WPF\Main_Window.xaml' -ActionsPath '.\UIActions\MainWindow\' -AddWPFVariables
 $WPF_Partition = Get-XAML -WPFPrefix 'WPF_DP_' -XMLFile '.\Assets\WPF\Grid_DiskPartition.xaml' -ActionsPath '.\UIActions\DiskPartition\' -AddWPFVariables
